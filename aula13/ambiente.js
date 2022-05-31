1c2a8aecd04ae55f78a80200fe02a8337860d7fc
CaioVRS21 <veras.caio12@gmail.com>
2022-05-31 11:09:03 -0400

    Atualização

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+let c = 1;
+do {
+    console.log('Tiozao eh mt fei kkkkk');
+    c++
+} while (c <=6 )
+
+
+
+// let c = 1;
+
+// while (c <= 1000){
+//     console.log('Tiozao eh fei dms kkkk');
+//     c++;
+// }
